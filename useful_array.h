cf6384c6c6baab52621aa4e134a29ce43a88a42b
junaid <mjunaid2017@namal.edu.pk>
2018-10-10 21:23:19 +0500

    finish task4

---
+++ useful_array.h
@@ -0,0 +1,8 @@
+#include <iostream>
+void reverse(int x[],int );
+void rotate(int x[],int ,int );
+int search(int x[],int ,int );
+void sort(int x[],int );
+int sortsearch(int x[],int ,int );
+int binarysearch(int x[],int ,int ,int );
+using namespace std;
